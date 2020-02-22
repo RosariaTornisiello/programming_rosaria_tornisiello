5081db7344f102ea60a3b9f9c3efc20d5dd4f2de
RosariaTornisiello <59016818+RosariaTornisiello@users.noreply.github.com>
2020-02-22 14:51:24 +0100

    Update NW_definitivo.py

--- exercises/NW_definitivo.py
+++ exercises/NW_definitivo.py
@@ -1,4 +1,4 @@
-def dictionary(file):				#define a function that creates a dictionary starting from the blosum62 square matrix
+def dictionary(file):		#define a function that creates a dictionary starting from the blosum62 square matrix
 	lista=[]
 	for line in file:
 		line=line.rstrip()
@@ -11,37 +11,37 @@ def dictionary(file):				#define a function that creates a dictionary starting f
 	return(dic)
 
 def NW(s1,s2,pen,dic):
-	scores=[]									#initialization of score and traceback matrices
+	scores=[]			#initialization of score and traceback matrices
 	trace_back=[]
 	N=len(s1)+1
 	M=len(s2)+1
-	scores=[[0]*(N) for i in range(M)]		 	#filling of the two matrices with 0		
+	scores=[[0]*(N) for i in range(M)]		 #filling of the two matrices with 0		
 	trace_back=[[0]*(N) for i in range(M)]
-	for i in range(1,M):						#filling of first row and first column of both the matrices						
+	for i in range(1,M):				#filling of first row and first column of both the matrices						
 		scores[i][0]=scores[i-1][0] + pen
 		trace_back[i][0]= 'u'
 	for j in range(1,N):
 		scores[0][j]=scores[0][j-1] + pen
 		trace_back[0][j]= 'l'
-	for i in range(1,M):        					#rows
-		for j in range(1,N):    					#columns
+	for i in range(1,M):        			#rows
+		for j in range(1,N):    		#columns
 			voc={}
-			up=scores[i-1][j]+ pen						#voc is a dictionary that associates up, left and diag to the values for each 
-			left=scores[i][j-1]+ pen							#position of the scores matrix
+			up=scores[i-1][j]+ pen	#voc is a dictionary that associates up, left and diag to the values for each 
+			left=scores[i][j-1]+ pen		#position of the scores matrix
 			diag=scores[i-1][j-1]+int(dic[s1[j-1]+s2[i-1]])
 			voc[up]='u'
 			voc[left]='l'
 			voc[diag]='d'
 			max_score=max(up,left,diag)
-			scores[i][j]=max_score								#fill each position of the scores matrix with the highest value
-			trace_back[i][j]=voc.get(max(up,left,diag))			#fill each position of the traceback matrix with u,l or d choosing the highest value
+			scores[i][j]=max_score		#fill each position of the scores matrix with the highest value
+			trace_back[i][j]=voc.get(max(up,left,diag))#fill each position of the traceback matrix with u,l or d choosing the highest value
 	return (scores, trace_back)									
 
-def align(s1,s2,trace_back,scores):
+def align(s1,s2,trace_back,scores): #funcion that return the alignment and the score
 	sal1=''
 	sal2=''
 	pipe=''
-	n=len(s1)									#allineamento con gaps e | 
+	n=len(s1)		 
 	m=len(s2)
 	while n!=0 or m!=0:
 		if trace_back[m][n]=='u':
@@ -63,7 +63,7 @@ def align(s1,s2,trace_back,scores):
 		sal1=sal1[::-1]
 		sal2=sal2[::-1]
 		pipe=pipe[::-1]
-		score=scores[m-1][n-1]					#score dell'allineamento
+		score=scores[m-1][n-1]		
 	return(sal1, pipe,sal2, score)
 
 
